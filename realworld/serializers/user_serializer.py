e114a4f77d247a8d66b1dda9f3910bde0e88ca91
fahrur <fahrurben@gmail.com>
2024-09-28 16:45:20 +0700

    Update User endpoint

--- realworld/serializers/user_serializer.py
+++ realworld/serializers/user_serializer.py
@@ -4,10 +4,11 @@ from realworld.models import CustomUser
 
 class UserSerializer(serializers.ModelSerializer):
     token = serializers.CharField(max_length=1000, read_only=True)
+    password = serializers.CharField(min_length=6, max_length=1000)
 
     class Meta:
         model = CustomUser
-        fields = ('email', 'token', 'username', 'bio', 'image')
+        fields = ('email', 'token', 'username', 'password', 'bio', 'image')
 
     def to_internal_value(self, data):
         resource_data = data['user']
--- realworld/views/user_view.py
+++ realworld/views/user_view.py
@@ -34,3 +34,24 @@ class UserView(APIView):
         user_serializer = UserSerializer(request.user)
         return Response(user_serializer.data, status=status.HTTP_200_OK)
 
+    def put(self, request):
+        if not request.user.is_authenticated:
+            return Response({}, status=status.HTTP_401_UNAUTHORIZED)
+
+        user = request.user
+        serializer = UserSerializer(data=request.data, partial=True)
+        if serializer.is_valid():
+            if 'password' in serializer.validated_data:
+                user.set_password(serializer.validated_data.pop('password'))
+
+            user.__dict__.update(serializer.validated_data)
+
+            user.save()
+
+            user_serializer = UserSerializer(request.user)
+            return Response(user_serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response({
+                'message': serializer.errors,
+                'data': None
+            }, status=status.HTTP_400_BAD_REQUEST)
